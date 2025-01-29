6d0591f91ca955d687d46382ee1ecd24ae9bff50
claudine756 <154498010+claudine756@users.noreply.github.com>
2025-01-28 22:44:55 -0800

    Update delmundo_tsa1.js

--- TSA1/delmundo_tsa1.js
+++ TSA1/delmundo_tsa1.js
@@ -1,1 +1,11 @@
-
+function changeColor(theme) {
+    document.body.classList.remove('pink2-theme', 'pink3-theme', 'pink1-theme');
+    
+    if (theme === 'pink2') {
+        document.body.classList.add('pink2-theme');
+    } else if (theme === 'pink3') {
+        document.body.classList.add('pink3-theme');
+    } else {
+        document.body.classList.add('pink1-theme');
+    }
+}
